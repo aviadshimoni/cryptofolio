476981551f2fcb61bb5f9dbfb5ffc48c79a0c705
aviads <aviad.shimoni@doubleverify.com>
2022-10-21 15:04:26 +0300

    aa

--- server/services/coin-service.js
+++ server/services/coin-service.js
@@ -4,15 +4,36 @@ const CoinMarketCap = require('coinmarketcap-api');
 exports.getCurrentPrice = (req, res) => {
   console.log(req.query);
   if (req.query) {
-    const coin_symbol = req.query.coin_symbol;
+    const coin_symbol = req.query.coinSymbol;
     try {
       const client = new CoinMarketCap(process.env.CMC_API_KEY);
-      client.getQuotes({ symbol: coin_symbol }).then((coin_quotes) => {
-        console.log(coin_quotes.data[coin_symbol].quote.USD.price);
-        res.send({ price: coin_quotes.data[coin_symbol].quote.USD.price });
+      client.getQuotes({ symbol: coinSymbol }).then((coin_quotes) => {
+        console.log(coin_quotes.data[coinSymbol].quote.USD.price);
+        res.send({ price: coin_quotes.data[coinSymbol].quote.USD.price });
       });
     } catch (error) {
       console.log(error);
     }
   }
+};
+
+exports.getTotalPortifolioWorth = (req, res) => {
+  axios
+      .get('http://localhost:3000/getUserBalan', { params: { userId: "63517abf96c1d8a1a8466ee6" } }) //ETH, BTC, XMR
+      .then(function (response) {
+        let sumCoinsObject = JSON.parse(response);
+        let totalPortifolioWorth = 0;
+        for(let i = 0; i < sumCoinsObject.line.length; i++)
+        {
+          let coin = sumCoinsObject.line[i].coin[0].shortName;
+          let amount = sumCoinsObject.line[i].amount;
+          const res = await axios.get('http://localhost:3000/api/sum', { params: { coinSymbol: coin  });
+          let currentPrice = res.data.price;
+          totalPortifolioWorth += currentPrice * amount;
+        }
+        res.send({ totalPortifolioWorth: totalPortifolioWorth })
+      })
+      .catch((err) => {
+        res.send(err);
+      });
 };
