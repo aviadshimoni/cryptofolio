1bdc7b634d531326204646e9096a1cfbf0a76356
Tzvika Tubis <tzvika.t@hiredscore.com>
2022-11-14 16:32:21 +0200

    fix contac

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,3 +1,7 @@
 function sendEmail(){
-    window.open('mailto:test@example.com?subject=subject&body=body');
+    var name = $('#contactForm :input[name=name]').val();
+    var subject = $('#contactForm :input[name=subject]').val();
+    var message = $('#contactForm :input[name=message]').val();
+
+    window.open(`mailto:cryptofolio@gmail.com?subject=${subject}&body=${message} %0D%0A ${name}`);
 }
