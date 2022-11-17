1fb1f44ed4b5eba948ce0a33030bdcdf68b6bfcf
omer5574 <55313275+omer5574@users.noreply.github.com>
2022-11-17 22:44:41 +0200

    fix about us js

--- assets/js/about.js
+++ assets/js/about.js
@@ -5,13 +5,3 @@ function sendEmail(){
 
     window.open(`mailto:cryptofolio@gmail.com?subject=${subject}&body=${message} %0D%0A ${name}`);
 }
-
-var canvas = document.getElementById("namesCanvas");
-var ctx = canvas.getContext("2d");
-ctx.strokeStyle = 'navy';
-ctx.font = "20px Verdana";
-ctx.fillStyle = "white";
-ctx.lineWidth = 4;
-ctx.textAlign = "center";
-ctx.strokeText("Aviad Shimoni | Tzvika Tubis | Omer Sharon | Moran Eshel ", 350, 20);
-ctx.fillText("Aviad Shimoni | Tzvika Tubis | Omer Sharon | Moran Eshel ", 350, 20);
