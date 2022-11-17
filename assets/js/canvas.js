b46e113cada8573aff7fb7a1072b5d13812e2dc5
omer5574 <55313275+omer5574@users.noreply.github.com>
2022-11-17 22:43:45 +0200

    add canvas

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -1,11 +1,3 @@
-function sendEmail(){
-    var name = $('#contactForm :input[name=name]').val();
-    var subject = $('#contactForm :input[name=subject]').val();
-    var message = $('#contactForm :input[name=message]').val();
-
-    window.open(`mailto:cryptofolio@gmail.com?subject=${subject}&body=${message} %0D%0A ${name}`);
-}
-
 var canvas = document.getElementById("namesCanvas");
 var ctx = canvas.getContext("2d");
 ctx.strokeStyle = 'navy';
